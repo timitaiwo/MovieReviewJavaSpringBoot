f995b90767a2aef96e577596b775b3be1f99ab93
Timi Taiwo <timi_taiwo@protonmail.com>
2024-10-20 19:50:41 +0100

    Initial commit

---
+++ src/main/java/com/timitaiwo/javaFSproject/JavaFSprojectApplication.java
@@ -0,0 +1,13 @@
+package com.timitaiwo.javaFSproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFSprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFSprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timitaiwo/javaFSproject/JavaFSprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timitaiwo.javaFSproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFSprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
