04af9018ffc7e9f04db8ce968053a73adc91a378
Timi Taiwo <timi_taiwo@protonmail.com>
2024-10-25 20:36:05 +0100

    Added movies and review models

---
+++ src/main/java/com/timitaiwo/javaFSproject/Models/Review.java
@@ -0,0 +1,20 @@
+package com.timitaiwo.javaFSproject.Models;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "reviews")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Review {
+
+    @Id
+    private ObjectId id;
+    private String body;
+}
